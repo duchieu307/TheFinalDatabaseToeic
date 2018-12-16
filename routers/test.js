8e5f9d19f860b512f31f170905adb793adec5973
duchieu307 <duchieu307@gmail.com>
2018-12-16 21:39:43 +0700

    Final 1

--- routers/test.js
+++ routers/test.js
@@ -761,7 +761,7 @@ Router.post("/test", async function (req, res) {
 
     await dtAnsArr.push(q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13, q14, q15, q16, q17, q18, q19, q20)
     console.log(dtAnsArr)
-    tnAnsArr.push(q21, q22, q23, q24, q25, q26, q27, q28, q29, q30, q31.q32, q33, q34, q35, q36, q37, q38, q39, q40)
+    await tnAnsArr.push(q21, q22, q23, q24, q25, q26, q27, q28, q29, q30, q31, q32, q33, q34, q35, q36, q37, q38, q39, q40)
     FileController.checkDienTu(dtAnsArr, (err, dt) => {
         let dtScore = dt
         console.log("Diem dien tu", dtScore)
